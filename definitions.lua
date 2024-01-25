712facffedac3ba759a014e8e37fa77aadf270ca
JustAPotota <justapotota.dev@gmail.com>
2024-01-25 15:49:32 -0600

    Add some things

---
+++ definitions.lua
@@ -0,0 +1,18 @@
+---@meta
+
+function printError(...) end
+
+peripheral = {}
+
+---@alias peripheral table
+
+---@param peripheral_type string
+---@param predicate fun(name: string, wrapped: peripheral)?
+---@return peripheral ...
+function peripheral.find(peripheral_type, predicate) end
+
+rednet = {}
+function rednet.open(modem) end
+
+---@return boolean
+function rednet.isOpen() end
---
+++ vortex_ui.lua
@@ -0,0 +1,53 @@
+local function getTardis()
+    return peripheral.find("vortexmod:vortex_interface_be")
+end
+
+---@return boolean
+local function openRednet()
+    peripheral.find("modem", rednet.open)
+    return rednet.isOpen()
+end
+
+---@return string?
+local function getHostname()
+    local file = io.open("hostname")
+    if not file then
+        return
+    end
+
+    local hostname = file:read("a")
+    file:close()
+
+    return hostname
+end
+
+---@param hostname string
+local function setHostname(hostname)
+    local file = io.open("hostname", "w")
+    if file then
+        file:write(hostname)
+        file:close()
+    end
+end
+
+local tardis = getTardis()
+if not tardis then
+    printError("Computer must be placed next to the Vortex Interface")
+    return
+end
+
+if not openRednet() then
+    printError("Unable to open rednet. Make sure there is a wireless or ender modem connected to this computer")
+end
+
+
+local basalt = require("basalt")
+
+local ui = basalt.createFrame()
+
+local button = ui:addButton():setPosition(4, 4):setSize(16, 3):setText("Click me!")
+button:onClick(function()
+    basalt.debug("Clicked")
+end)
+
+basalt.autoUpdate()
